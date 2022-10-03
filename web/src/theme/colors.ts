73e6664b55515cf3ec5614e9bac0e039b353684b
Yun <broody@gmail.com>
2022-10-02 18:22:57 -0700

    chore: added chakra theme (#2)

---
+++ web/src/theme/colors.ts
@@ -0,0 +1,14 @@
+export default { 
+  whiteAlpha: {
+    200: "rgba(255, 255, 255, 0.08)",
+    400: "rgba(255, 255, 255, 0.24)",
+    600: "rgba(255, 255, 255, 0.48)",
+    800: "rgba(255, 255, 255, 0.80)",
+  },
+  blackAlpha: {
+    600: "rgba(0, 0, 0, 0.48)",
+  },
+  white: "#FFF",
+  black: "#000",
+  transparent: "transparent",
+}
